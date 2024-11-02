3e1b027eb9e37ac0bad719507296ccd7e51e3efb
edwin24o <edwinolivares24@gmail.com>
2024-11-02 17:26:57 -0500

    added cruds for customers

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from typing import List
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column
+from marshmallow import ValidationError
+from sqlalchemy import Column, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 import os
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
@@ -13,8 +15,10 @@ class Base(DeclarativeBase):
     pass 
 
 db = SQLAlchemy(model_class = Base)
+ma = Marshmallow()
 
 db.init_app(app)
+ma.init_app(app)
 
 service_mechanics = db.Table(
     "service_mechanics",
@@ -23,7 +27,7 @@ service_mechanics = db.Table(
     Column("mechanic_id", db.ForeignKey("mechanics.id"))
 )
 
-class customers(Base):
+class Customer(Base):
     __tablename__ = 'customers'
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -31,7 +35,7 @@ class customers(Base):
     email: Mapped[str] = mapped_column(db.String(100), nullable=False, unique=True)
     phone: Mapped[str] = mapped_column(db.String(20))
 
-class service_tickets(Base):
+class Service_ticket(Base):
     __tablename__ = 'service_tickets'
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -40,7 +44,7 @@ class service_tickets(Base):
     service_desc: Mapped[str] = mapped_column(db.String(1500), nullable=False)
     customer_id: Mapped[int] = mapped_column(db.ForeignKey('customers.id'))
 
-class mechanics(Base):
+class Mechanic(Base):
     __tablename__ = 'mechanics'
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -48,6 +52,90 @@ class mechanics(Base):
     email: Mapped[str] = mapped_column(db.String(100), nullable=False, unique=True)
     phone: Mapped[str] = mapped_column(db.String(20), nullable=False )
     salary: Mapped[float] = mapped_column(db.Float(7), nullable=False)
+
+
+# ================== Schemas =====================
+
+class CustomerSchema(ma.Schema):
+    class Meta:
+        model = Customer
+
+class ServiceticketSchema(ma.Schema):
+    class Meta:
+        model = Service_ticket
+
+class MechanicSchema(ma.Schema):
+    class Meta:
+        model = Mechanic
+
+customer_schema = CustomerSchema()
+customers_schema = CustomerSchema(many=True)
+
+serviceticket_schema = ServiceticketSchema()
+servicetickets_schema = ServiceticketSchema(many=True)
+
+mechanic_schema = MechanicSchema()
+mechanics_schema = MechanicSchema(many=True)
+
+# =================== Routes ===================
+
+# /customers POST: Create Customer
+@app.route("/customers", methods=['POST'])
+def create_customer():
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400 
+    
+    new_customer = Customer(name=customer_data['name'], email=customer_data['email'], phone=customer_data['phone'])
+    db.session.add(new_customer)
+    db.session.commit()
+
+    return customer_schema.jsonify(new_customer), 201
+
+# /customers GET: Get all customers
+@app.route("/customers", methods=['GET'])
+def create_customer():
+    query = select(Customer)
+    customers = db.session.execute(query).scalars().all()
+    return customer_schema.jsonify(customers), 200
+
+# /customers/<id> GET: Get customer belonging to id
+@app.route("/customers/<int:id>", methods=['GET'])
+def get_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+    return customer_schema.jsonify(customer), 200
+
+#  /customers/<id> PUT: Update Customer
+@app.route("/customers/<int:id>", methods=['PUT'])
+def uodate_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+
+    if customer == None:
+        return jsonify({"message": "invalid id"}), 400
+    
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    for field, value in customer_data.items():
+        setattr(customer, field, value)
+    
+    db.session.commit()
+    return customer_schema.jsonify(customer), 200
+
+# /customers/<id> DELETE: Delete Customer
+@app.route("/customers/<int:id>", methods=['DELETE'])
+def delete_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+
+    if customer == None:
+        return jsonify({'message': "invalid id" }), 400
+    
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": f"succefuly deleted user {customer_id}!"})
 
 
 if __name__ == '__main__':
