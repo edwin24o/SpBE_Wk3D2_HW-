e74f78ab49e39f955cfbb820daccdd95f3a68d4f
edwin24o <edwinolivares24@gmail.com>
2024-11-02 16:12:27 -0500

    installed marshmallow and added to req.txt

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
+from typing import List
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-import os #link to the os.environ.get to retrieve the uri link
+from sqlalchemy import Column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+import os
 from datetime import date
-from sqlalchemy import Column   
-
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') #used to hide sensitive information 
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 class Base(DeclarativeBase):
-    pass
+    pass 
 
-db= SQLAlchemy(model_class= Base)
+db = SQLAlchemy(model_class = Base)
 
 db.init_app(app)
 
@@ -28,7 +27,7 @@ class customers(Base):
     __tablename__ = 'customers'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(db.String(80), nullable=False)##VARCHAR IN SQL BUT STR IN PYTHON
+    name: Mapped[str] = mapped_column(db.String(80), nullable=False)
     email: Mapped[str] = mapped_column(db.String(100), nullable=False, unique=True)
     phone: Mapped[str] = mapped_column(db.String(20))
 
@@ -47,7 +46,7 @@ class mechanics(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(80), nullable=False)
     email: Mapped[str] = mapped_column(db.String(100), nullable=False, unique=True)
-    phone: Mapped[str] = mapped_column(db.String(20), nullable=False ) # requried for contact purpose 
+    phone: Mapped[str] = mapped_column(db.String(20), nullable=False )
     salary: Mapped[float] = mapped_column(db.Float(7), nullable=False)
 
 
