4b0bb06d240900337e64f0a92611ef9a49fd96d9
edwin24o <edwinolivares24@gmail.com>
2024-11-03 08:27:25 -0600

    aded inlcudefk

--- application/blueprints/servicetickets/schemas.py
+++ application/blueprints/servicetickets/schemas.py
@@ -2,15 +2,20 @@ from application.extension import ma
 from application.models import Service_ticket
 from marshmallow import fields
 
-class ServiceSchema(ma.Schema):
-    id = fields.Integer(required=False)
-    customer_id = fields.Integer(required=True)
-    service_ticket_date = fields.Date(required=True)
-    due_date = fields.Date(required=True)
+# class ServiceSchema(ma.Schema):
+#     id = fields.Integer(required=False)
+#     customer_id = fields.Integer(required=True)
+#     service_ticket_date = fields.Date(required=True)
+#     due_date = fields.Date(required=True)
 
 
+#     class Meta:
+#         fields = ('customer_id', 'service_ticket_date', 'due_date', 'id')
+
+class ServiceSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = ('customer_id', 'service_ticket_date', 'due_date', 'id')
+        model = Service_ticket
+        include_fk = True
 
 service_ticket_schema = ServiceSchema()
 input_service_ticket_schema = ServiceSchema(exclude=['service_ticket_date', 'due_date'])
