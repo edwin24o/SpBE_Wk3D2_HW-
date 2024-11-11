73faa89c3ce27ab2b979706d2caf26e9232bd3e3
edwin24o <edwinolivares24@gmail.com>
2024-11-10 21:02:50 -0600

    done

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,12 @@ from application.extension import ma, limiter, cache
 from application.blueprints.customers import customers_bp
 from application.blueprints.servicetickets import service_tickets_bp
 from application.blueprints.mechanics import mechanics_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'
+API_URL = "/static/swagger.yaml"
+
+swagger_bp = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={"app_name": "Mechanic API"})
 
 def create_app(config_name):
     
@@ -18,5 +24,6 @@ def create_app(config_name):
     app.register_blueprint(customers_bp, url_prefix='/customers')
     app.register_blueprint(service_tickets_bp, url_prefix='/service_tickets')
     app.register_blueprint(mechanics_bp, url_prefix='/mechanics')
+    app.register_blueprint(swagger_bp, url_prefix=SWAGGER_URL)
 
     return app
